7b8ea0586961a8df36db4de653b595112ae9d97c
Hailemariyam <hailemariyam3298@gmail.com>
2025-03-31 16:17:37 +0300

    new update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://degan-live.vercel.app"],
+    origin: ["https://tubular-salmiakki-aae177.netlify.app/"],
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
     credentials: true,
