99103aef751ff8f3849231a9be0fe0b662e338fe
Hailemariyam <hailemariyam3298@gmail.com>
2025-03-31 17:02:54 +0300

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://tubular-salmiakki-aae177.netlify.app/"],
+    origin: ["https://degan-live.netlify.app/"],
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
     credentials: true,
